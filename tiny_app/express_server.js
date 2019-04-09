af469edee1ebff01b101f1b969eb9bbe53396607
Andrea <anmastrantoni@gmail.com>
2019-04-09 16:52:08 +0000

    added views folder

--- tiny_app/express_server.js
+++ tiny_app/express_server.js
@@ -8,9 +8,11 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase}
+  res.render("urls_index", templateVars);
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
